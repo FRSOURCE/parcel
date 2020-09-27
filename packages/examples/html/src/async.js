73089b3b2e7d7da20fbb4d41c5bfffabf2828c4d
Jakub Freisler <jakubfreisler@phrase.com>
2020-09-27 19:55:41 +0200

    chore: show bug when importing html template in js module

--- packages/examples/html/src/async.js
+++ packages/examples/html/src/async.js
@@ -1,6 +1,11 @@
 console.log(require('react'));
 require('lodash');
+import a from './otherCpy.html';
+import * as b from './otherCpy.html';
 import './child.css';
+console.log(a);
+console.log(b);
+console.log(require('./otherCpy.html'));
 console.log('async');
 
 class Foo {}
